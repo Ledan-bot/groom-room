103bc895cc04a9f87142e300518faa799a317eb4
Nicholas Ledan <Nickledan5@gmail.com>
2022-01-08 14:20:50 -0500

    init

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/Ledan-bot/groom-room/server/controllers"
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	setupServer().Run(":8070")
+}
+
+func setupServer() *gin.Engine {
+	router := gin.Default()
+	router.Use(static.Serve("/", static.LocalFile("./public", true)))
+	router.GET("/api/test", controllers.HandleTest)
+	return router
+}
